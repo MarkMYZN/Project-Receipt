d809d16e9957b5e31aacae4c3d46aaacb4ca1c89
Mark <141608680+MarkMYZN@users.noreply.github.com>
2023-12-12 10:04:04 +0800

    Update Receipt_Main.c

--- Receipt_Main.c
+++ Receipt_Main.c
@@ -20,10 +20,25 @@ void displayFunc() {
             printf("You are in Beverage Aisle.\n");
             printf("Items in shelf:\n");
             beverage();
-
+        case 2: // beverage aisle
+            system("cls");
+            printf("You are in Beverage Aisle.\n");
+            printf("Items in shelf:\n");
+            beverage();
+        case 3: // beverage aisle
+            system("cls");
+            printf("You are in Beverage Aisle.\n");
+            printf("Items in shelf:\n");
+            beverage();
+        case 4: // beverage aisle
+            system("cls");
+            printf("You are in Beverage Aisle.\n");
+            printf("Items in shelf:\n");
+            beverage();
     }
 }
 
+//Aisles
 void beverage() {
     printf("(1) Product 1..........$2\n");
     printf("(2) Product 2..........$3\n");
@@ -32,7 +47,6 @@ void beverage() {
     printf("(5) Product 5..........$2\n");
     beverageFunc();
 }
-
 void beverageFunc() {
     int bev;
     scanf("%d", &bev);
@@ -85,12 +99,252 @@ void beverageFunc() {
             break;
     }
 }
+void cleaning() {
+    printf("(1) Product 1..........$2\n");
+    printf("(2) Product 2..........$3\n");
+    printf("(3) Product 3..........$1\n");
+    printf("(4) Product 4..........$4\n");
+    printf("(5) Product 5..........$2\n");
+    cleaningFunc();
+}
+void cleaningFunc() {
+    int clean;
+    scanf("%d", &clean);
+
+    switch (clean) {
+        case 1: 
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 2:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 3:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 4:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 5:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+    }
+}
+void personal() {
+    printf("(1) Product 1..........$2\n");
+    printf("(2) Product 2..........$3\n");
+    printf("(3) Product 3..........$1\n");
+    printf("(4) Product 4..........$4\n");
+    printf("(5) Product 5..........$2\n");
+    personalFunc();
+}
+void personalFunc() {
+    int personl;
+    scanf("%d", &personl);
+
+    switch (personl) {
+        case 1: 
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 2:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 3:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 4:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 5:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+    }
+}
+void breakfast() {
+    printf("(1) Product 1..........$2\n");
+    printf("(2) Product 2..........$3\n");
+    printf("(3) Product 3..........$1\n");
+    printf("(4) Product 4..........$4\n");
+    printf("(5) Product 5..........$2\n");
+    breakfastFunc();
+}
+void breakfastFunc() {
+    int brkfst;
+    scanf("%d", &brkfst);
+
+    switch (brkfst) {
+        case 1: 
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 2:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 3:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 4:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 5:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+    }
+}
+void frozen() {
+    printf("(1) Product 1..........$2\n");
+    printf("(2) Product 2..........$3\n");
+    printf("(3) Product 3..........$1\n");
+    printf("(4) Product 4..........$4\n");
+    printf("(5) Product 5..........$2\n");
+    frozenFunc();
+}
+void frozenFunc() {
+    int froze;
+    scanf("%d", &froze);
+
+    switch (froze) {
+        case 1: 
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 2:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 3:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 4:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+        case 5:
+            system("cls");
+            printf("Item Added to Cart.\n");
+            printf("\nReady to head out?\n");
+            printf("(8) Go back to lobby\n");
+            printf("(9) Go to checkout\n");
+            printf("Enter your choice: ");
+            exitLobby();
+            break;
+    }
+}
 
 void checkoutFunc() {
     system("cls");
     printf("You are in Checkout Area.\n");
     printf("Printing receipt....\nPlease wait...\n");
-    printf("\n\n\n\n\nTHIS IS THE RECEIPT\n\n\n\n\n");
+    printf("placholdder for receipt");
     printf("\nThank you for shopping with us!\n");
     getchar();
 }
